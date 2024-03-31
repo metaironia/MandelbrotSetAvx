b39ba3f281da1dc158ad8dd1047df056a980b182
metaironia <serezha364@gmail.com>
2024-03-31 17:13:50 +0300

    added sensible no simd computation

--- src/mandelbrot_computation.cpp
+++ src/mandelbrot_computation.cpp
@@ -4,19 +4,18 @@
 #include "graphics.h"
 #include "mandelbrot_computation.h"
 
-ComputationFunc MandelbrotComputeNoSIMD (RGBQUAD *videomem) {
+#define FOR_ACCUM  for (size_t i = 0; i < ACCUM_NUM; i++)
+ComputationFunc MandelbrotComputeSillyNoSIMD (RGBQUAD *videomem) {
 
     assert (videomem);
 
     float y_0 = OFFSET_AXIS_Y;
 
-    for (size_t y_pixel = 0; y_pixel < WINDOW_SIZE_Y; y_pixel++,
-                                                      y_0 += DELTA_Y) {
+    for (size_t y_pixel = 0; y_pixel < WINDOW_SIZE_Y; y_pixel++, y_0 += DELTA_Y) {
         
         float x_0 = OFFSET_AXIS_X;
             
-        for (size_t x_pixel = 0; x_pixel < WINDOW_SIZE_X; x_pixel++,
-                                                          x_0 += DELTA_X) {
+        for (size_t x_pixel = 0; x_pixel < WINDOW_SIZE_X; x_pixel++, x_0 += DELTA_X) {
 
             float x = x_0;
             float y = y_0;
@@ -37,6 +36,80 @@ ComputationFunc MandelbrotComputeNoSIMD (RGBQUAD *videomem) {
             }
 
             PixelColorSet (videomem, x_pixel, y_pixel, iter_num);
+        }
+    }
+
+    return COMPUTATION_FUNC_STATUS_OK;
+}
+
+ComputationFunc MandelbrotComputeSensibleNoSIMD (RGBQUAD *videomem) {
+
+    assert (videomem);
+
+    float y_0 = OFFSET_AXIS_Y;
+
+    for (size_t y_pixel = 0; y_pixel < WINDOW_SIZE_Y; y_pixel++, y_0 += DELTA_Y) {
+        
+        float x_0[ACCUM_NUM] = {OFFSET_AXIS_X,               OFFSET_AXIS_X + DELTA_X, 
+                                OFFSET_AXIS_X + DELTA_X * 2, OFFSET_AXIS_X + DELTA_X * 3};
+            
+        for (size_t x_pixel = 0; x_pixel < WINDOW_SIZE_X; x_pixel += ACCUM_NUM) {
+
+            float x[ACCUM_NUM] = {};
+            float y[ACCUM_NUM] = {};
+
+            FOR_ACCUM
+                x[i] = x_0[i];
+
+            FOR_ACCUM
+                y[i] = y_0;
+
+            size_t iter_num[ACCUM_NUM] = {};
+
+            for (size_t curr_iter = 0; curr_iter <= MAX_COMPUTATION_NUM; curr_iter++) {
+                
+                float curr_x_sq[ACCUM_NUM] = {};
+                float curr_y_sq[ACCUM_NUM] = {};
+                float curr_xy[ACCUM_NUM]   = {}; 
+
+                FOR_ACCUM
+                    curr_x_sq[i] = x[i] * x[i];
+                
+                FOR_ACCUM
+                    curr_y_sq[i] = y[i] * y[i];
+                
+                FOR_ACCUM
+                    curr_xy[i] = x[i] * y[i];
+
+                int pixel_mask = 0;
+
+                int is_dot_out[ACCUM_NUM] = {};
+
+                FOR_ACCUM
+                    if ((curr_x_sq[i] + curr_y_sq[i]) > MANDELBROT_RADIUS_SQUARE)
+                        is_dot_out[i] = 1;
+                
+                FOR_ACCUM
+                    pixel_mask |= (is_dot_out[i] << i);
+
+                if (!(pixel_mask ^ ACCUM_MASK))
+                    break;
+
+                FOR_ACCUM
+                    x[i] = curr_x_sq[i] - curr_y_sq[i] + x_0[i];
+                
+                FOR_ACCUM
+                    y[i] = 2 * curr_xy[i] + y_0; 
+
+                FOR_ACCUM
+                    iter_num[i] += 1 - is_dot_out[i];
+            }
+
+            FOR_ACCUM
+                PixelColorSet (videomem, x_pixel + i, y_pixel, iter_num[i]);
+
+            FOR_ACCUM
+                x_0[i] += ACCUM_NUM * DELTA_X;
         }
     }
 
--- src/mandelbrot_computation.h
+++ src/mandelbrot_computation.h
@@ -18,9 +18,13 @@ const float OFFSET_AXIS_X = -2.f;
 const float OFFSET_AXIS_Y = -1.f;
 
 const float MANDELBROT_RADIUS_SQUARE = 100.f;
+const int   MAX_COMPUTATION_NUM      = 256;
 
-const int MAX_COMPUTATION_NUM = 256;
+const int ACCUM_NUM  = 4;
+const int ACCUM_MASK = 0xF;
 
-ComputationFunc MandelbrotComputeNoSIMD (RGBQUAD *videomem);
+ComputationFunc MandelbrotComputeSillyNoSIMD (RGBQUAD *videomem);
+
+ComputationFunc MandelbrotComputeSensibleNoSIMD (RGBQUAD *videomem);
 
 #endif
